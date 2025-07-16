5336cf28dc6aa04e39d33f7654a8cbbb1062faf0
Manikandan <simbumani18@gmail.com>
2025-07-16 21:59:43 +0530

    best seller working on

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../components/Banner";
 import Categories from "../components/Categories";
+import BestSeller from "../components/BestSeller";
 
 export const Home = () => {
   return (
     <div className="mt-10">
       <Banner />
       <Categories />
+      <BestSeller />
     </div>
   );
 };
