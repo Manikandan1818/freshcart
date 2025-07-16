51d2a2e63e15ffac23ceb06cddd0a8ca6ae2e16c
Manikandan <simbumani18@gmail.com>
2025-07-16 18:24:44 +0530

    Categories comp completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../components/Banner";
+import Categories from "../components/Categories";
 
 export const Home = () => {
   return (
     <div className="mt-10">
       <Banner />
+      <Categories />
     </div>
   );
 };
