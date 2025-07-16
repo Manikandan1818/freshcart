a7bcdd2bbe99d69f77a699515abf3431d437e73d
Manikandan <simbumani18@gmail.com>
2025-07-16 17:52:59 +0530

    banner comp updated!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     <div>
       {isSellerPath ? null : <Navbar />}
       <div
-        className={{ isSellerPath } ? "" : `px-6 md:px-16 lg:px-24 xl:px-32`}
+        className={`${isSellerPath} ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
       >
         <Routes>
           <Route path="/" element={<Home />} />
