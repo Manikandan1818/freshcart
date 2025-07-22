524aa2688c105d43019a5632a408ba93c0e73495
Manikandan <simbumani18@gmail.com>
2025-07-22 17:12:05 +0530

    bottom banner image inserted!

--- client/src/components/BottomBanner.jsx
+++ client/src/components/BottomBanner.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { assets } from "../assets/assets";
 
 const BottomBanner = () => {
-  return <div>BottomBanner</div>;
+  return (
+    <div className="relative mt-24">
+      <img
+        src={assets.bottom_banner_image}
+        alt="banner"
+        className="w-full hidden md:block"
+      />
+    </div>
+  );
 };
 
 export default BottomBanner;
