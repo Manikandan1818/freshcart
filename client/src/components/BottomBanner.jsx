6777133f1e1a18ec3e966a3013f6405126b70da6
Manikandan <simbumani18@gmail.com>
2025-07-22 18:07:41 +0530

    bottom banner completed

--- client/src/components/BottomBanner.jsx
+++ client/src/components/BottomBanner.jsx
@@ -1,4 +1,4 @@
-import { assets } from "../assets/assets";
+import { assets, features } from "../assets/assets";
 
 const BottomBanner = () => {
   return (
@@ -8,6 +8,31 @@ const BottomBanner = () => {
         alt="banner"
         className="w-full hidden md:block"
       />
+      <img
+        src={assets.bottom_banner_image_sm}
+        alt="banner"
+        className="w-full md:hidden"
+      />
+      <div className="absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24">
+        <div>
+          <h1 className="text-2xl md:text-3xl font-semibold text-primary mb-6">
+            Why We Are the Best?
+          </h1>
+          {features.map((feature, index) => (
+            <div key={index} className="flex items-center gap-2 mt-4">
+              <img src={feature.icon} alt={feature.title} className="" />
+              <div>
+                <h3 className="text-lg md:text-xl font-semibold">
+                  {feature.title}
+                </h3>
+                <p className="text-grey-500/70 text-xs md:text-sm">
+                  {feature.description}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
