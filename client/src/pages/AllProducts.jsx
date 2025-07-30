c19bc8df1c98e61599d0c7adf3d29277e5a352b1
Manikandan <simbumani18@gmail.com>
2025-07-30 20:56:09 +0530

    working on all product

--- client/src/pages/AllProducts.jsx
+++ client/src/pages/AllProducts.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { useAppContext } from "../context/AppContext";
+import { useState } from "react";
 
 const AllProducts = () => {
-  return <div>AllProducts</div>;
+  const { products } = useAppContext();
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  return (
+    <div className="mt-16 flex flex-col">
+      <div>
+        <p>All Products</p>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default AllProducts;
