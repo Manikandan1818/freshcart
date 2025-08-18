172b567368d2fd199877d1fee0c1b4ee88cc023b
Manikandan <simbumani18@gmail.com>
2025-08-18 22:17:10 +0530

    get cart amont and count function crreated on Appcontext

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -64,6 +64,19 @@ export const AppContextProvider = ({ children }) => {
     return totalCount;
   };
 
+  // Get Cart Total Amount
+
+  const getCartAmount = () => {
+    let totalAmount = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find((product) => product._id === items);
+      if (cartItems[items] > 0) {
+        totalAmount += itemInfo.offerPrice * cartItems[items];
+      }
+    }
+    return Math.floor(totalAmount * 100) / 100;
+  };
+
   useEffect(() => {
     fetchProducts();
   }, []);
@@ -84,6 +97,8 @@ export const AppContextProvider = ({ children }) => {
     removeFromCart,
     searchQuery,
     setSearchQuery,
+    getCartCount,
+    getCartAmount,
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
