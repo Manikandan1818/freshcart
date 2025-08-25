56cec05fffa4b90784326c27af02c53538fd9cb9
Manikandan <simbumani18@gmail.com>
2025-08-25 16:19:59 +0530

    working on cart addresses

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useAppContext } from "../context/AppContext";
-import { dummyAddress } from "../assets/assets";
+import { assets, dummyAddress } from "../assets/assets";
 
 const Cart = () => {
   const {
@@ -20,11 +20,30 @@ const Cart = () => {
   const [selectedAddress, setSelectedAddress] = useState(dummyAddress[0]);
   const [paymentOption, setPaymentOption] = useState("COD");
 
-  return (
-    <div className="flex flex-col md:flex-row py-16 max-w-6xl w-full px-6 mx-auto">
+  const getCart = () => {
+    let tempArray = [];
+    for (const key in cartItems) {
+      const product = find((item) => item._id === key);
+      product.quantity = cartItems[key];
+      tempArray.push(product);
+    }
+    setCartArray(tempArray);
+  };
+
+  useEffect(() => {
+    if (products.length > 0 && cartItems) {
+      getCart();
+    }
+  }, [products, cartItems]);
+
+  return products.length > 0 && cartItems ? (
+    <div className="flex flex-col md:flex-row mt-16">
       <div className="flex-1 max-w-4xl">
         <h1 className="text-3xl font-medium mb-6">
-          Shopping Cart <span className="text-sm text-indigo-500">3 Items</span>
+          Shopping Cart{" "}
+          <span className="text-sm text-indigo-500">
+            {getCartCount()} Items
+          </span>
         </h1>
 
         <div className="grid grid-cols-[2fr_1fr_1fr] text-gray-500 text-base font-medium pb-3">
@@ -33,16 +52,24 @@ const Cart = () => {
           <p className="text-center">Action</p>
         </div>
 
-        {products.map((product, index) => (
+        {cartArray.map((product, index) => (
           <div
             key={index}
             className="grid grid-cols-[2fr_1fr_1fr] text-gray-500 items-center text-sm md:text-base font-medium pt-3"
           >
             <div className="flex items-center md:gap-6 gap-3">
-              <div className="cursor-pointer w-24 h-24 flex items-center justify-center border border-gray-300 rounded overflow-hidden">
+              <div
+                onClick={() => {
+                  navigate(
+                    `/products/${product.category.toLowerCase()}/${product._id}`
+                  );
+                  scrollTo(0, 0);
+                }}
+                className="cursor-pointer w-24 h-24 flex items-center justify-center border border-gray-300 rounded overflow-hidden"
+              >
                 <img
                   className="max-w-full h-full object-cover"
-                  src={product.image}
+                  src={product.image[0]}
                   alt={product.name}
                 />
               </div>
@@ -50,12 +77,14 @@ const Cart = () => {
                 <p className="hidden md:block font-semibold">{product.name}</p>
                 <div className="font-normal text-gray-500/70">
                   <p>
-                    Size: <span>{product.size || "N/A"}</span>
+                    Weight: <span>{product.weight || "N/A"}</span>
                   </p>
                   <div className="flex items-center">
                     <p>Qty:</p>
                     <select className="outline-none">
-                      {Array(5)
+                      {Array(
+                        cartItems[product._id] > 9 ? cartItems[product._id] : 9
+                      )
                         .fill("")
                         .map((_, index) => (
                           <option key={index} value={index + 1}>
@@ -68,44 +97,34 @@ const Cart = () => {
               </div>
             </div>
             <p className="text-center">
-              ${product.offerPrice * product.quantity}
+              {currency}
+              {product.offerPrice * product.quantity}
             </p>
-            <button className="cursor-pointer mx-auto">
-              <svg
-                width="20"
-                height="20"
-                viewBox="0 0 20 20"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="m12.5 7.5-5 5m0-5 5 5m5.833-2.5a8.333 8.333 0 1 1-16.667 0 8.333 8.333 0 0 1 16.667 0"
-                  stroke="#FF532E"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
+            <button
+              onClick={() => removeFromCart()}
+              className="cursor-pointer mx-auto"
+            >
+              <img
+                src={assets.refresh_icon}
+                alt="remove"
+                className="inline-block w-6 h-6"
+              />
             </button>
           </div>
         ))}
 
-        <button className="group cursor-pointer flex items-center mt-8 gap-2 text-indigo-500 font-medium">
-          <svg
-            width="15"
-            height="11"
-            viewBox="0 0 15 11"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M14.09 5.5H1M6.143 10 1 5.5 6.143 1"
-              stroke="#615fff"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
+        <button
+          onClick={() => {
+            navigate("/products");
+            scrollTo(0, 0);
+          }}
+          className="group cursor-pointer flex items-center mt-8 gap-2 text-indigo-500 font-medium"
+        >
+          <img
+            src={assets.arrow_right_icon_colored}
+            alt="arrow"
+            className="group-hover:-translate-x-1 transition"
+          />
           Continue Shopping
         </button>
       </div>
@@ -176,6 +195,6 @@ const Cart = () => {
         </button>
       </div>
     </div>
-  );
+  ) : null;
 };
 export default Cart;
