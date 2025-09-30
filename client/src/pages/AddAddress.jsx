3d3234b87ef814ae9903acee51229ccf3536b3ee
Manikandan <simbumani18@gmail.com>
2025-09-30 17:28:00 +0530

    Add address comp completed

--- client/src/pages/AddAddress.jsx
+++ client/src/pages/AddAddress.jsx
@@ -1,20 +1,26 @@
 import React, { useState } from "react";
 import { assets } from "../assets/assets";
+// import InputField from "../components/Input";
 
-// Input Field Component
-
-const InputField = ({ type, placeholder, name, handleChange, address }) => {
-  <input
-    className="w-full px-2 py-2.5 border border-gray-500/30 rounded outline-none text-gray-500 focus:border-primary transition"
-    type={type}
-    placeholder={placeholder}
-    onChange={handleChange}
-    name={name}
-    address={address[name]}
-    required
-  />;
+export const InputField = ({
+  type,
+  placeholder,
+  name,
+  handleChange,
+  address,
+}) => {
+  return (
+    <input
+      className="w-full px-2 py-2.5 border border-gray-500/30 rounded outline-none text-gray-500 focus:border-primary transition"
+      type={type}
+      placeholder={placeholder}
+      onChange={handleChange}
+      name={name}
+      address={address[name]}
+      required
+    />
+  );
 };
-
 const AddAddress = () => {
   const [address, setAddress] = useState({
     firstName: "",
@@ -32,7 +38,7 @@ const AddAddress = () => {
     const { name, value } = e.target;
 
     setAddress((prevAddress) => ({
-      ...AddAddress,
+      ...prevAddress,
       [name]: value,
     }));
   };
@@ -72,6 +78,55 @@ const AddAddress = () => {
               type="email"
               placeholder="Email address"
             />
+            <InputField
+              handleChange={handleChange}
+              address={address}
+              name="street"
+              type="text"
+              placeholder="Street"
+            />
+            <div className="grid grid-cols-2 gap-4">
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="city"
+                type="text"
+                placeholder="City"
+              />
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="state"
+                type="text"
+                placeholder="State"
+              />
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="zipcode"
+                type="number"
+                placeholder="Zip code"
+              />
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="country"
+                type="text"
+                placeholder="Country"
+              />
+            </div>
+            <InputField
+              handleChange={handleChange}
+              address={address}
+              name="phone"
+              type="text"
+              placeholder="Phone"
+            />
+            <button className="w-full mt-6 bg-primary text-white py-3 hover:bg-primary-dull transition cursor-pointer uppercase">
+              Save address
+            </button>
           </form>
         </div>
         <img
