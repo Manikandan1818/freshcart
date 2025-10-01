a2e8e185662509972822729fae3dfe37cf095142
Manikandan <simbumani18@gmail.com>
2025-10-01 20:58:32 +0530

    getting dummy orders form assests

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAppContext } from "../context/AppContext";
+import { dummyAddress } from "../assets/assets";
 
 const MyOrders = () => {
+  const [myOrders, setMyOrders] = useState([]);
+  const { currency } = useAppContext();
+
+  const fetchOrders = async () => {
+    setMyOrders(dummyAddress);
+  };
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
   return (
-    <div>
-      <p>MyOrders</p>
+    <div className="mt-16 pb-16">
+      <div className="flex flex-col items-end w-max mb-8">
+        <p className="text-2xl font-medium uppercase">MyOrders</p>
+        <div className="w-16 h-0.5 bg-primary rounded-full"></div>
+      </div>
+      {myOrders.map((order, index) => (
+        <div>
+          <p>
+            <span>OrderId: {order._id}</span>
+            <span>Payment: {order.paymentType}</span>
+            <span>
+              Total Amount: {currency} {order.amont}
+            </span>
+          </p>
+        </div>
+      ))}
     </div>
   );
 };
