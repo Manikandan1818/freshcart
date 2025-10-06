230a10ecadd4e17f2c52af599b27fb7da41825f4
Manikandan <simbumani18@gmail.com>
2025-10-06 17:40:01 +0530

    working on seller login form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import ProductDetails from "./pages/ProductDetails";
 import Cart from "./pages/Cart";
 import AddAddress from "./pages/AddAddress";
 import MyOrders from "./pages/MyOrders";
+import SellerLogin from "./components/seller/SellerLogin";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
@@ -33,6 +34,10 @@ const App = () => {
           <Route path="/cart" element={<Cart />} />
           <Route path="/add-address" element={<AddAddress />} />
           <Route path="/my-orders" element={<MyOrders />} />
+          <Route
+            path="/seller"
+            element={isSellerPath ? null : <SellerLogin />}
+          />
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
