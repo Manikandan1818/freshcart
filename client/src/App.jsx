01552de7c638ef0bc5846f0d1df9f862b1848cee
Manikandan Dhandabhani <mdhandabhani@questel.com>
2025-11-20 21:47:50 +0530

    seller layout margin issue solved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
       {showUserLogin ? <Login /> : null}
       <Toaster />
       <div
-        className={`${isSellerPath} ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
+        className={`${isSellerPath ? "" : "md:px-16 lg:px-24 xl:px-32"}`}
       >
         {/* md:px-16 lg:px-24 xl:px-32 */}
         <Routes>
