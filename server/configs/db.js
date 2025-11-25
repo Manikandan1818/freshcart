36bddb7aaf765f14a9cc3baffb6583c7d0436901
Manikandan Dhandabhani <mdhandabhani@questel.com>
2025-11-25 23:03:01 +0530

    problem in db connection

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,11 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        mongoose.connection.on("Connected", () => {console.log("Database Connected")})
-        await mongoose.connect(`${process.env.MONGODB_URL}`)
+        mongoose.connection.on("connected", ()=> {console.log("Database Connected")})
+        await mongoose.connect(`${process.env.MONGODB_URL}/Cluster0`)
     } catch (error) {
         console.log(error.message)
     }
+    
 }
 
 export default connectDB
