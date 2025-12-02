9e95948c5b65eada25bfb957d3ae8051807a5616
Manikandan Dhandabhani <mdhandabhani@questel.com>
2025-12-02 17:18:15 +0530

    Finally database connected -- error mongourl is double quotes

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         mongoose.connection.on("connected", ()=> {console.log("Database Connected")})
-        await mongoose.connect(process.env.MONGODB_URL)
+        await mongoose.connect(process.env.MONGODB_URL)                
     } catch (error) {
         console.log(error.message)
     }
-    
+            
 }
 
 export default connectDB
