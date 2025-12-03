2cc44797d225b6bf9b9389cbac807d53613fbaf6
Manikandan Dhandabhani <mdhandabhani@questel.com>
2025-12-03 21:55:51 +0530

    user route completed

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from "express"
 import cors from "cors"
 import connectDB from "./configs/db.js";
 import "dotenv/config.js"
+import userRouter from "./routes/userRoute.js";
 
 const app = express();
 const port = process.env.PORT || "4000";
@@ -20,10 +21,10 @@ app.use(cors({origin: allowedOrigins, credentials: true}))
 
 
 app.get("/", (req, res)=> res.send("API is running..."))
+app.use("/api/user", userRouterer)
+
 app.listen(port, ()=> {
   console.log(`Server is running on http://localhost:${port}`)
 })
-
-
 
 
