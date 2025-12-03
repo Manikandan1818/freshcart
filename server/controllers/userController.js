ae5b7a0747267c7a12964856e1813a17f3bb8b0e
Manikandan Dhandabhani <mdhandabhani@questel.com>
2025-12-03 19:57:26 +0530

    working on usercontroller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,26 @@
+import jwt from "jsonwebtoken"
+import User from "../models/User"
+import bcrypt from "bcryptjs"
+
+// Register user: /api/user/register
 export const register = async (req, res) => {
     try {
         const {name, email, password } = req.body
+        if(!name || !email || !password){
+            res.json({sucess: false, message: "Missing details"})
+        }
+        
+        const existingUser = await User.findone({email})
+        if(existingUser){
+            return res.json({sucess: false, message: "User already exits"})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const user = await User.create({name, email, password:hashedPassword})
+
+        const token = jwt.sign({id: user._id,})
+
     } catch (error) {
         
     }
