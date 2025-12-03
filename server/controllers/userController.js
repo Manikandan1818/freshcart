555b8f58879e45de757eecbd8d46e9142c03b74e
Manikandan Dhandabhani <mdhandabhani@questel.com>
2025-12-03 21:39:02 +0530

    User controller js completed

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -19,10 +19,19 @@ export const register = async (req, res) => {
 
         const user = await User.create({name, email, password:hashedPassword})
 
-        const token = jwt.sign({id: user._id,})
+        const token = jwt.sign({id: user._id, process.env.JWT_SECRET}, {expiresIn: "7d"})
+
+        res.cookie("token", {
+            httpOnly: true, // Prevent Javascript to access cookie
+            secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", // CSRF Production
+            maxAge: 7 * 24 * 60 * 100, //Cookie expiration time
+        })
+
+        return res.json({sucess: true, user: {name: user.name, email: user.email}})
 
     } catch (error) {
-        
+        console.log(error.message)
+        res.json({sucess: false, message: error.message})
     }
-
 }
