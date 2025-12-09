65f31c22656c48f074d061d439a8f324ad4e0621
Manikandan Dhandabhani <mdhandabhani@questel.com>
2025-12-09 21:41:06 +0530

    working on authuser

--- server/middlewares/authUser.js
+++ server/middlewares/authUser.js
@@ -1,4 +1,10 @@
 
 const authUser = async (req, res, next) => {
+    
+    const {token} = req.cookies;
+
+    if(!token) {
+        return res.json({sucess: false, message: "Not Authorized"})
+    }
 
 }
