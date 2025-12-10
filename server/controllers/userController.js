5aa787069066fb8c84806c661ca84a893881f53f
Manikandan Dhandabhani <mdhandabhani@questel.com>
2025-12-10 22:10:28 +0530

    working on is auth function in user controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -74,4 +74,10 @@ export const login = async (req, res) => {
 
 // Check Auth : /api/user/is-auth
 
-export const isAuth = () => {}
+export const isAuth = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
