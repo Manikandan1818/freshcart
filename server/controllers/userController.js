a7ee9b3a6c66bbb532a49c03142f6f14e085df0f
Manikandan Dhandabhani <mdhandabhani@questel.com>
2025-12-18 21:19:05 +0530

    working on user controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -78,7 +78,7 @@ export const isAuth = async (req, res) => {
     try {
         const {userId} = req.body      
         const user = await User.findById(userId).select("-password")
-        return res.json({sucess: true})
+        return res.json({sucess: true, user})
 
     } catch (error) {
         
