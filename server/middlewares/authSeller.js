e2ba692e43260fe343f07c736bd4c80a3c204527
Manikandan Dhandabhani <mdhandabhani@questel.com>
2026-01-08 18:06:18 +0530

    working on auth seller

--- server/middlewares/authSeller.js
+++ server/middlewares/authSeller.js
@@ -2,4 +2,14 @@ import jwt from "jsonwebtoken"
 
 const authSeller = async (req, res, next) => {
     const {sellerToken} = res.cookies;
+
+    if(!sellerToken){
+        return res.json({sucess: false, message: "Not Authorized"})
+    }
+
+    try {
+        const tokenDecode = jwt.verify(sellerToken, process.env.JWT_SECRET)
+    } catch (error) {
+        
+    }
 }
