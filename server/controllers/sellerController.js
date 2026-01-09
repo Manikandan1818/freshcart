a515804ed7750d2a519d580b360e42e214356633
Manikandan Dhandabhani <mdhandabhani@questel.com>
2026-01-09 19:39:24 +0530

    working on seller controller

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -8,9 +8,19 @@ export const sellerLogin = async (req, res) => {
 
         if(password === process.env.SELLER_PASSWORD && email === process.env.SELLER_EMAIL){
             const token = jwt.sign({email}, process.env.JWT_SECRET, {expiresIn: "7d"})
-        }    
+            res.cookie("sellerToken", token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+                maxAge: 7 * 24 * 60 * 60 * 1000,
+            })
+            return res.json({sucess: true, message: "Logged In"})
+        }else{
+            return res.json({sucess: false, message: "Invalid Credentials"})
+        }
 
     } catch (error) {
-        
+        console.log(error.message)
+        res.json({sucess: false, message: error.message})        
     }
 }
