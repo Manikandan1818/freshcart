3ca3ad3c7af75356a13adc1d2f8401d18f939504
Manikandan Dhandabhani <mdhandabhani@questel.com>
2026-01-09 19:32:03 +0530

    working on seller controller

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -2,4 +2,15 @@ import jwt from "jsonwebtoken"
 
 // Login Seller : /api/seller/login
 
-export const sellerLogin = async (req, res) => {}
+export const sellerLogin = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        if(password === process.env.SELLER_PASSWORD && email === process.env.SELLER_EMAIL){
+            const token = jwt.sign({email}, process.env.JWT_SECRET, {expiresIn: "7d"})
+        }    
+
+    } catch (error) {
+        
+    }
+}
