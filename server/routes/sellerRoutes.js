d2a801cc4bc0c00426a41d34490a51e130961112
Manikandan Dhandabhani <mdhandabhani@questel.com>
2026-01-13 15:32:45 +0530

    Seller route js created!

---
+++ server/routes/sellerRoutes.js
@@ -0,0 +1,9 @@
+import express from "express"
+import { isSellerAuth, sellerLogin, sellerLogout } from "../controllers/sellerController.js";
+import authSeller from "../middlewares/authSeller.js";
+
+const sellerRouter = express.Router();
+
+sellerRouter.post("/", sellerLogin)
+sellerRouter.get("/is-auth", authSeller, isSellerAuth)
+sellerRouter.get("/logout", sellerLogout)
