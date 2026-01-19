be1b7dc9b82906653cc4d153aa978f76fd1746fd
Manikandan Dhandabhani <mdhandabhani@questel.com>
2026-01-19 19:34:26 +0530

    created add, list, single product, instocl func created in productController

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,1 +1,19 @@
 // Add product: /api/product/add
+export const addProduct = async (req, res) => {
+
+}
+
+// Get Product: /api/product/list
+export const productList = async (req, res) => {
+
+}
+
+// Get single : /api/product/id
+export const productById = async (req, res) => {
+
+}
+
+// Get product instock : /api/product/id
+export const changeStock = async (req, res) => {
+
+}
