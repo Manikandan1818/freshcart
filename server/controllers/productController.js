ede52e1bea01ac66c8a8898965a505e42bdfed79
Manikandan Dhandabhani <mdhandabhani@questel.com>
2026-01-27 12:03:14 +0530

    working on productController

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,9 +1,17 @@
+import {v2 as cloudinary} from "cloudinary"
+
 // Add product: /api/product/add
 export const addProduct = async (req, res) => {
     try {
         let productData = JSON.parse(req.body.productData)
 
         const images = req.files
+        let imageURL = await Promise.all(
+            images.map(async (item) => {
+                let result = await cloudinary.uploader.upload(item.path, {resource_type: "image"})
+                return result.secure_url
+            })
+        )
         
     } catch (error) {
         
