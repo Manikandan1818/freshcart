47af8e27b36b81456ceb617dfb34fbb1d1c5a0c3
Manikandan Dhandabhani <mdhandabhani@questel.com>
2026-01-30 21:16:59 +0530

    cartRouter comp  created

---
+++ server/routes/cartRoute.js
@@ -0,0 +1,14 @@
+// import mongoose from "mongoose";
+
+// const cartRouter = mongoose.Router()
+
+
+import express from "express"
+import authUser from "../middlewares/authUser"
+import { updateCart } from "../controllers/cartController"
+
+const cartRouter = express.Router()
+
+cartRouter.post("/update", authUser, updateCart)
+
+export default cartRouter
