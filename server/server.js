57be69519926c1c9421b00740b59e60705c0e36b
Manikandan Dhandabhani <mdhandabhani@questel.com>
2026-02-05 22:18:12 +0530

    address model and router completed

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import sellerRouter from "./routes/sellerRoutes.js";
 import connectCloudinary from "./configs/cloudinary.js";
 import productRouter from "./routes/productRoutes.js";
 import cartRouter from "./routes/cartRoute.js";
+import addressRouter from "./routes/addressRoutr.js";
 
 const app = express();
 const port = process.env.PORT || "4000";
@@ -29,6 +30,7 @@ app.use("/api/user", userRouter)
 app.use("/api/seller", sellerRouter)
 app.use("/api/product", productRouter)
 app.use("/api/cart", cartRouter)
+app.use("/api/address", addressRouter)
 
 app.listen(port, ()=> {
   console.log(`Server is running on http://localhost:${port}`)
