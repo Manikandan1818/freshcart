b6ea5d0702d281cb11bed0865e74e5b61ef1a9d4
Manikandan Dhandabhani <mdhandabhani@questel.com>
2026-02-06 20:56:58 +0530

    orderrouter api created

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -46,3 +46,16 @@ export const getUserOrders = async (req, res) => {
         res.json({success:false, message: error.message})
     }
 }
+
+// Get all orders by (admin/seller)
+
+export const getAllOrders = async (req, res) => {
+    try {        
+        const orders = Order.find({
+            $or: [{paymentType: "COD"}, {isPaid: true}]
+        }).populate("items.product, address").sort({createdAt: -1})
+        res.json({success:true, orders})
+    } catch (error) {
+        res.json({success:false, message: error.message})
+    }
+}
