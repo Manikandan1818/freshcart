966e618c9e12405de4f763ffcfe28e01d8f2c1ed
Manikandan <simbumani18@gmail.com>
2026-04-03 13:40:44 +0530

    Cloudniary images upload issue resolved

--- client/src/pages/seller/AddProduct.jsx
+++ client/src/pages/seller/AddProduct.jsx
@@ -24,7 +24,6 @@ const AddProduct = () => {
         price,
         offerPrice
       }
-
       const formData = new FormData();
       formData.append("productData", JSON.stringify(productData))
       for (let i = 0; i < files.length; i++) {
