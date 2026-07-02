a63e8566f26b7a3abea1cb8cb8e0bd0aa49fe39b
Manikandan <simbumani18@gmail.com>
2026-07-02 23:01:21 +0530

    issue with fetching data in mongoDB

--- server/server.js
+++ server/server.js
@@ -25,7 +25,6 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({origin: allowedOrigins, credentials: true}))
 
-
 app.get("/", (req, res)=> res.send("API is running..."))
 app.use("/api/user", userRouter)
 app.use("/api/seller", sellerRouter)
